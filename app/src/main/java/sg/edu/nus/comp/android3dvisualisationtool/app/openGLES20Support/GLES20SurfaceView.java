e69bfdbd58eee0910e56474f825400bdefc5de40
Pan Long <aga.panlong@me.com>
2014-06-12 13:53:32 +0800

    fix the conflict between drag and scale

--- app/src/main/java/sg/edu/nus/comp/android3dvisualisationtool/app/openGLES20Support/GLES20SurfaceView.java
+++ app/src/main/java/sg/edu/nus/comp/android3dvisualisationtool/app/openGLES20Support/GLES20SurfaceView.java
@@ -2,6 +2,7 @@ package sg.edu.nus.comp.android3dvisualisationtool.app.openGLES20Support;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
@@ -50,50 +51,75 @@ public class GLES20SurfaceView extends GLSurfaceView {
 
     private float mPreviousX;
     private float mPreviousY;
+    private final int INVALID_POINTER_ID = -1;
+    private int mActivePointerId = INVALID_POINTER_ID;
 
     @Override
-    public boolean onTouchEvent(MotionEvent e) {
-        // MotionEvent reports input details from the touch screen
-        // and other input controls. In this case, you are only
-        // interested in events where the touch position changed.
+    public boolean onTouchEvent(MotionEvent ev) {
+        // Let the ScaleGestureDetector inspect all events.
+        detector.onTouchEvent(ev);
 
-        float x = e.getX(), y = e.getY();
+        final int action = MotionEventCompat.getActionMasked(ev);
 
-        switch (e.getAction()) {
-            case MotionEvent.ACTION_POINTER_DOWN:
-                mode = ZOOM;
-                break;
+        switch (action) {
+            case MotionEvent.ACTION_DOWN: {
+                final int pointerIndex = MotionEventCompat.getActionIndex(ev);
+                final float x = MotionEventCompat.getX(ev, pointerIndex);
+                final float y = MotionEventCompat.getY(ev, pointerIndex);
 
-            case MotionEvent.ACTION_DOWN:
+                // Remember where we started (for dragging)
                 mPreviousX = x;
                 mPreviousY = y;
+                // Save the ID of this pointer (for dragging)
+                mActivePointerId = MotionEventCompat.getPointerId(ev, 0);
                 break;
+            }
 
-            case MotionEvent.ACTION_MOVE:
-                int count = e.getPointerCount();
-                if (count == 1) {
-                    mode = ROTATE;
-                    mRenderer.setRotation((int)mPreviousX, (int)mPreviousY, (int)x, (int)y);  // = 180.0f / 320
-                    mPreviousX = x;
-                    mPreviousY = y;
+            case MotionEvent.ACTION_MOVE: {
+                // Find the index of the active pointer and fetch its position
+                final int pointerIndex =
+                        MotionEventCompat.findPointerIndex(ev, mActivePointerId);
+
+                final float x = MotionEventCompat.getX(ev, pointerIndex);
+                final float y = MotionEventCompat.getY(ev, pointerIndex);
+
+                mRenderer.setRotation((int)mPreviousX, (int)mPreviousY, (int)x, (int)y);
+
+                requestRender();
+
+                // Remember this touch position for the next move event
+                mPreviousX = x;
+                mPreviousY = y;
+
+                break;
+            }
+
+            case MotionEvent.ACTION_UP: {
+                mActivePointerId = INVALID_POINTER_ID;
+                break;
+            }
+
+            case MotionEvent.ACTION_CANCEL: {
+                mActivePointerId = INVALID_POINTER_ID;
+                break;
+            }
+
+            case MotionEvent.ACTION_POINTER_UP: {
+
+                final int pointerIndex = MotionEventCompat.getActionIndex(ev);
+                final int pointerId = MotionEventCompat.getPointerId(ev, pointerIndex);
+
+                if (pointerId == mActivePointerId) {
+                    // This was our active pointer going up. Choose a new
+                    // active pointer and adjust accordingly.
+                    final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
+                    mPreviousX = MotionEventCompat.getX(ev, newPointerIndex);
+                    mPreviousY = MotionEventCompat.getY(ev, newPointerIndex);
+                    mActivePointerId = MotionEventCompat.getPointerId(ev, newPointerIndex);
                 }
                 break;
-
-            case MotionEvent.ACTION_POINTER_UP:
-                mode = ROTATE;
-                mRenderer.setRotation((int)mPreviousX, (int)mPreviousY, (int)x, (int)y);  // = 180.0f / 320
-                mPreviousX = x;
-                mPreviousY = y;
-                break;
-
+            }
         }
-
-        detector.onTouchEvent(e);
-
-        if (mode == ROTATE || mode == ZOOM) {
-            requestRender();
-        }
-
         return true;
     }
 
